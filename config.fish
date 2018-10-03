288adbea0848b5837423e8f9d34108c84f57b3cc
dat14mak <dat14mak@student.lu.se>
2018-10-03 19:22:19 +0200

    fish config

---
+++ config.fish
@@ -0,0 +1,21 @@
+set PATH $HOME/.cargo/bin /usr/local/bin /usr/bin /usr/sbin /sbin $PATH
+#set -Ux RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
+#set -Ux LDFLAGS "-L/usr/local/opt/llvm/lib"
+#set -Ux CPPFLAGS "-I/usr/local/opt/llvm/include"
+#set -Ux LIBRARY_PATH "$LIBRARY_PATH:/opt/local/lib/"
+set -Ux LIBRARY_PATH ""
+set -Ux EDITOR nvim
+
+set -Ux GOPATH $HOME/Programming/go
+
+alias vim nvim
+
+alias aceproxy "docker run -t -p 8000:8000 ikatson/aceproxy"
+alias efishrc "nvim ~/.config/fish/config.fish"
+alias evimrc "nvim ~/.vimrc"
+#set -U FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+set -Ux FZF_DEFAULT_COMMAND "fd --type file --color=always"
+set -Ux FZF_FIND_FILE_COMMAND "fd --type file --color=always"
+set -Ux FZF_DEFAULT_OPTS '--ansi'
+set -Ux FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -Ux FZF_CTRL_T_OPTS $FZF_DEFAULT_OPTS
