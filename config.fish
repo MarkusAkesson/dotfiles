fcc4bdaac1ea028cacbcf3000db73bc741a86fb6
Markus Åkesson <macke.95_06@hotmail.com>
2021-03-10 19:58:18 +0100

    use keychain, starship

--- config.fish
+++ config.fish
@@ -7,16 +7,16 @@ set fish_function_path $fish_function_path /usr/share/powerline/fish
 set -Ux EDITOR nvim
 
 # make Java applications happy within DWM
-#set -Ux _JAVA_AWT_WN_NONPARENTING 1
-#set -Ux AWT_TOOLKIT MToolkit
-#wmname LG3D
+set -Ux _JAVA_AWT_WN_NONPARENTING 1
+set -Ux AWT_TOOLKIT MToolkit
+wmname LG3D
 
-#set -Ux GOPATH /me2/repos/go
+set -Ux GOPATH /me2/repos/go
 
 alias vim nvim
 
 # Cmd aliases
-#alias aceproxy "docker run -t -p 8000:8000 ikatson/aceproxy"
+alias vim nvim
 alias efishrc "nvim ~/.config/fish/config.fish"
 alias evimrc "nvim ~/.vimrc"
 alias eterm "nvim ~/.config/alacritty/alacritty.yml"
@@ -24,11 +24,10 @@ alias ls "ls -hl --color"
 alias gotop ytop
 
 # Launch aliases
-#alias cutter "~/bin/Cutter-v1.8.1-x64.Linux.AppImage"
-#alias ghidra "~/repos/ghidra_9.0.1/ghidraRun"
-#alias matlab "/usr/local/MATLAB/R2019a/bin/matlab"
-#alias intellij "~/bin/idea-IC-191.6707.61/bin/idea.sh"
+# alias ghidra "~/repos/ghidra_9.0.1/ghidraRun"
+alias cutter "~/.bin/Cutter-v1.12.0-x64.Linux.AppImage"
 
+# FZF
 set -Ux FZF_DEFAULT_COMMAND 'rg --files --color auto --ignore --hidden  --glob "!.git/*"'
 #set -Ux FZF_DEFAULT_COMMAND "fd --type file --color=always"
 #set -Ux FZF_FIND_FILE_COMMAND "fd --type file --color=always"
@@ -36,11 +35,16 @@ set -Ux FZF_DEFAULT_OPTS '--ansi'
 set -Ux FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -Ux FZF_CTRL_T_OPTS $FZF_DEFAULT_OPTS
 
-# Default rusflags, generates faster binaries
-# set -Ux RUSTFLAGS "-C target-cpu=native"
-
 # Enable powerline fonts
 # powerline-setup
 
+# Prepare keychain
+if status --is-interactive
+    keychain --quiet -Q --eval id_rsa id_ed25519 | source
+end
+
+# Dev
+set -Ux CC clang
+set -Ux CXX clang++
 # Launch the starship prompt
-eval (starship init fish)
+starship init fish | source
